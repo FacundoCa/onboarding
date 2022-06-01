dec5696ce4bc25d7eba58d97a8cf3e37e062ffed
FacundoCa <fcanaspallero@gmail.com>
2022-05-31 23:09:12 -0300

    challenge.html

---
+++ Practicas/copia challenge/main.js
@@ -0,0 +1,14 @@
+
+
+//
+const buttonElement = document.querySelector('#search');
+const InputElement = document.querySelector('#inputValue');
+
+
+
+
+
+buttonElement = function (event) {
+    event.preventDefault();
+    console.log('hola');
+}  
