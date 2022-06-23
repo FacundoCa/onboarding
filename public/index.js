52a58a974a3082ab2f7e74cda4c1a097bff81af8
FacundoCa <fcanaspallero@gmail.com>
2022-06-22 21:25:00 -0300

    index.html

--- public/index.js
+++ public/index.js
@@ -1,2 +1,35 @@
 "use strict";
-console.log("estamos compilando");
+const toFrom = document.querySelector('#toFromInput');
+const creditDebit = document.querySelector('#creditDebitSelect');
+const amount = document.querySelector('#amountInput');
+const details = document.querySelector('#details');
+const sendEmail = document.querySelector('#sendEmail');
+const form = document.querySelector('#form');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const transaction = {
+        creditDebit: creditDebit.value,
+        toFrom: toFrom.value,
+        amount: amount.valueAsNumber,
+        details: details.value,
+        sendEmail: sendEmail.checked,
+    };
+    renderTransaction(transaction);
+});
+const renderTransaction = (t) => {
+    const tableRow = document.createElement('tr');
+    const creditDebitData = document.createElement('td');
+    creditDebitData.innerHTML = t.creditDebit;
+    const toFromData = document.createElement('td');
+    toFromData.innerHTML = t.toFrom;
+    const amountData = document.createElement('td');
+    amountData.innerHTML = t.amount.toString();
+    const detailsData = document.createElement('td');
+    detailsData.innerHTML = t.details;
+    tableRow.append(creditDebitData);
+    tableRow.append(toFromData);
+    tableRow.append(amountData);
+    tableRow.append(detailsData);
+    const tBody = document.querySelector('tbody');
+    tBody.prepend(tableRow);
+};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,56 @@
-console.log("estamos compilando");
+const toFrom = document.querySelector('#toFromInput') as HTMLInputElement;
+const creditDebit = document.querySelector('#creditDebitSelect') as HTMLInputElement;
+const amount = document.querySelector('#amountInput') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLTextAreaElement;
+const sendEmail = document.querySelector('#sendEmail') as HTMLInputElement
+
+const form = document.querySelector('#form') as HTMLFormElement;
+
+interface transactionInterface {
+    creditDebit: string,
+        toFrom: string,
+        amount: number, 
+        details: string, 
+        sendEmail: boolean
+}
+
+
+form.addEventListener('submit',(event) => {
+    event.preventDefault();
+
+    const transaction: transactionInterface = {
+        creditDebit: creditDebit.value, 
+        toFrom: toFrom.value,
+        amount: amount.valueAsNumber, 
+        details: details.value, 
+        sendEmail:sendEmail.checked, 
+    }
+
+    renderTransaction(transaction)
+    
+})
+
+const renderTransaction = (t: transactionInterface) => {
+    const tableRow = document.createElement('tr');
+
+    const creditDebitData = document.createElement('td');
+    creditDebitData.innerHTML = t.creditDebit;
+
+    const toFromData = document.createElement('td');
+    toFromData.innerHTML = t.toFrom;
+
+    const amountData = document.createElement('td');
+    amountData.innerHTML = t.amount.toString();
+
+    const detailsData = document.createElement('td');
+    detailsData.innerHTML = t.details;
+
+    tableRow.append(creditDebitData)
+    tableRow.append(toFromData)
+    tableRow.append(amountData)
+    tableRow.append(detailsData)
+
+    const tBody = document.querySelector('tbody')!;
+
+    tBody.prepend(tableRow)
+}
