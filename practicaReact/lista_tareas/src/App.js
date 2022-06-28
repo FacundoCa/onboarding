840f16214728b2568c91992de5029283e30dec7a
FacundoCa <fcanaspallero@gmail.com>
2022-06-28 00:34:33 -0300

    index.html

--- practicaReact/lista_tareas/src/App.js
+++ practicaReact/lista_tareas/src/App.js
@@ -4,14 +4,62 @@ import './App.css';
 
 class App extends Component {
   state = {
-    todoTitle: "Aprender JavaScript"
+    todos: [
+      {title: "aprender JavaScript", completed: false},
+      {title: "aprender CSS", completed: false},
+      {title: "aprender React", completed: false}
+    ],
+    inputValue: " "
   }
 
-  handleClick = () => {
-    this.setState({todoTitle: "Aprender HTML"})
+ toggleTodo = (e) => {
+  const index = e.target.getAttribute ("data-index")
+  const todos = [...this.state.todos];
+  const todo = {...todos[index]};
+  todo.completed = !todo.completed;
+  todos[index] = todo;
+  this.setState({todos: todos})
+ }
+
+  deleteTodo= (e) => {
+    const index = e.target.getAttribute ("data-index")
+    console.log("borrar tarea", index)
+    const todos = [...this.state.todos];
+    todos.splice(index, 1);
+    this.setState({
+      todos: todos 
+    })
+  }
+
+  renderTodos = () => {
+    return this.state.todos.map(
+      (todo, index) => {
+        return (
+            <li  key={todo.title}>
+
+              <span className={`title-tarea ${todo.completed ?"completed" : " "}`}>{todo.title}</span>
+              <span role="img" area-aria-label="emoji" className='icon-tarea' onClick={this.toggleTodo} data-index={index}>🆗</span>
+              <span role="img" area-aria-label="emoji" className='icon-tarea' onClick={this.deleteTodo} data-index={index}>❌</span>
+            </li>
+        )
+      }
+    )
+    
+  }
+
+  submitHandler = (e) => {
+    e.preventDefault();
+    const newTodos = {
+      title: this.state.inputValue,
+      completed: false
+    };
+    this.setState({todos: [...this.state.todos, newTodos],
+    inputValue: " "})
   }
 
   render () {
+
+    const renderedTodos = this.renderTodos()
     return ( 
       <div className="App">
         <div className='container'>
@@ -19,26 +67,15 @@ class App extends Component {
           Lista de tareas
           </h1>
           <ul>
-            <li className='title-tarea'>
-
-            {this.state.todoTitle}
-              <span role="img" area-aria-label="emoji" className='icon-tarea' onClick={this.handleClick}>🆗</span>
-              <span role="img" area-aria-label="emoji" className='icon-tarea'>❌</span>
-            </li>
-            <li className='title-tarea'>
-            Aprender CSS
-              <span role="img" area-aria-label="emoji" className='icon-tarea' onClick={this.handleClick}>🆗</span>
-              <span role="img" area-aria-label="emoji" className='icon-tarea'>❌</span>
-            </li>
-            <li className='title-tarea'>
-
-            Aprender React
-              <span role="img" area-aria-label="emoji" className='icon-tarea' onClick={this.handleClick}>🆗</span>
-              <span role="img" area-aria-label="emoji" className='icon-tarea'>❌</span>
-            </li>
+            {renderedTodos}
           </ul>
-          <form>
-            <input type="text" placeholder='ingrese una nueva tarea'></input>
+          <form onSubmit={this.submitHandler}>
+            <input 
+            type="text" 
+            placeholder='ingrese una nueva tarea'
+            value={this.state.inputValue}
+            onChange={e=> this.setState({inputValue: e.target.value})}
+            ></input>
             <button><span role="img" area-aria-label="emoji">➕</span></button>
           </form>
           
@@ -51,10 +88,3 @@ class App extends Component {
 
 export default App;
 
-
-
-//lista de tareas
-//boton de marcar ok
-//boton de eliminar 
-//caja de texto
-//boton de agregar
