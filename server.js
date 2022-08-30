cbf1c31f5424b3b57a681770b1b7cecf0884c79c
FacundoCa <fcanaspallero@gmail.com>
2022-08-30 19:35:18 -0300

    index.html

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require('express');
 const server = express();
 
-server.listen(3000, () => {
-    console.log('Servidor iniciado en el puerto 3000');
-});
-
 server.get('/json', (req, res) => {
     const json = {
         nombre: "facu",
